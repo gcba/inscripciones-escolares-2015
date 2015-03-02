b5818095243c14406aa755a8441c9d250ca49bcd
Davo <davidgalavotti@gmail.com>
2015-03-02 19:18:33 -0300

    Agrego choropleth del mapa de comunas

--- js/mapa_de_caba.js
+++ js/mapa_de_caba.js
@@ -1,3 +1,13 @@
+var nivel = "secundario";
+
+var grillaSvg = {
+  ancho: 960,
+  alto: 470,
+  labelSpace: 70,
+  filasGeneral: 9,
+  columnasGeneral: 15,
+  columnasPorNivel: 4
+}
 
 var mapaSvg = {
   ancho: 300,
@@ -5,6 +15,8 @@ var mapaSvg = {
   posInicialX: grillaSvg.ancho,
   posInicialY: grillaSvg.alto - grillaSvg.labelSpace - 300
 }
+
+
 
 // Crear SVG para los circulos
 var svg = d3.select("#mapaCABA")
@@ -14,10 +26,39 @@ var svg = d3.select("#mapaCABA")
          .attr("x", mapaSvg.posInicialX)
          .attr("y", mapaSvg.posInicialY);
 
+queue()
+    .defer(d3.json, "data/comunas.json")
+    .defer(d3.json, "data/data.json")
+    .await(ready);
 
-d3.json("data/comunas.json", function(error, caba) {
-  svg.append("path")
-        .datum(topojson.feature(caba, caba.objects.comunas))
-        .attr("d", d3.geo.path().projection(d3.geo.mercator().scale(157000/2).center([-58.20000,-34.68102])))
-        .attr('class', 'comuna');
-});
+function ready(error, comunas, data) {
+
+  var color = d3.scale.linear()
+    .domain([500, 1500, 2000, 2800, 3800])
+    .range(["#f2f0f7", "#dadaeb", "#bcbddc", "#9e9ac8", "#756bb1", "#54278f"]);
+
+  // function calcularColor(nivel){
+  //   color = d3.scale.linear()
+  //   .domain([500, d3.max(data.comunas[nivel],function(d){ return parseInt(d[0]); }) ])
+  //   .range(["#f2f0f7", "#dadaeb", "#bcbddc", "#9e9ac8", "#756bb1", "#54278f"]);
+  //   console.log(d3.max(data.comunas.inicial));
+  // }
+
+  console.log(data.comunas[nivel]);
+
+  // console.log(d3.max(data));
+  // console.log(nivel);
+  // console.log(d3.min(data.comunas[nivel]));
+
+  // console.log(data.comunas.inicial);
+
+  svg.append("g")
+      .attr("class", "caba")
+    .selectAll("path")
+      .data(topojson.feature(comunas, comunas.objects.comunas).features)
+    .enter().append("path")
+      .attr("d", d3.geo.path().projection(d3.geo.mercator().scale(157000/2).center([-58.20000,-34.68102])))
+      .style("fill", function(d) { return color(data.comunas[nivel][d.properties.comuna]);
+      });
+
+}
